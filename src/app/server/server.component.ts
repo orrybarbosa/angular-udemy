ac41fbd06665c947b52d1ae07774018ab8edcfe6
Orry De Sousa Barbosa <orry@Orrys-MacBook-Pro.local>
2017-08-31 10:11:02 +0200

    added my-app

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
+})
+export class ServerComponent implements OnInit {
+
+	constructor() { 
+	}
+
+	ngOnInit() {
+	}
+
+	serverId: number = 10;
+	serverStatus: string = "Offline";
+
+}
